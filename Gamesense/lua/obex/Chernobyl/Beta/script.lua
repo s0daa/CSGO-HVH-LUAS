95f62a6dada87401ee9891ccd791d4b0b0732ba2
soda <70986459+s0daa@users.noreply.github.com>
2025-04-13 20:31:57 -0500

    rename some stuff and remove empty folders in obex

--- Gamesense/lua/obex/Chernobyl/Beta/script.lua
+++
@@ -1,4 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-print('this is a demo script since your lua provider hasnt updated yet :)')
