b2fbdb72cb23ac6c981a15b860a316f5509f3ee1
soda <70986459+s0daa@users.noreply.github.com>
2025-04-14 08:15:12 -0500

    rename

--- Gamesense/lua/!!!!!!!!!!!!!obex/Hysteria/Private/script.lua
+++
@@ -1,6 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-local inspect = require "inspect"
-
-print( inspect(getfenv()) )
