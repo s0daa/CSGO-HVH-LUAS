1614475c97d122dae9cb174c1f221ee9d3def3a7
soda <70986459+s0daa@users.noreply.github.com>
2025-04-14 08:31:40 -0500

    zzz

--- Gamesense/lua/!!!!!!!!!!!!!obex/Kenzo/Debug/script.lua
+++
@@ -1,4 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-print("Open a ticket and request your build to be updated, show this message in ticket")
--- Gamesense/lua/!!!!!!!!!!!!!obex/Kenzo/User/script.lua
+++
@@ -1,4 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-print("Recode in progress, follow updates via discord. You have USER build, if you believe this to be a mistake please create a ticket.")
