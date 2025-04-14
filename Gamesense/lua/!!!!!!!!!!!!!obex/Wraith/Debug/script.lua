732f54dfd61ab4d65ac2138499d2752f9530509e
soda <70986459+s0daa@users.noreply.github.com>
2025-04-14 13:55:50 -0500

    rename all obex luas and make second obex folder

--- Gamesense/lua/!!!!!!!!!!!!!obex/Wraith/Debug/script.lua
+++
@@ -1,4 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-print("moving loader, don't panic, check announcements @discord.gg/lby - wraith by raz")
--- Gamesense/lua/!!!!!!!!!!!!!obex/Wraithsemi/Beta/script.lua
+++
@@ -1,4 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-print('moving loader, look out for updates on our discord')
