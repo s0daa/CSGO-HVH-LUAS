030399cb99c1ec23fcac468791ed54062aa8dd11
soda <70986459+s0daa@users.noreply.github.com>
2025-04-14 08:11:22 -0500

    why am i doing this

--- Gamesense/lua/!!!!!!!!!!!!!obex/Galaxy/Debug/script.lua
+++
@@ -1,4 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-print("contact me on discord lumiel#0001")
--- Gamesense/lua/!!!!!!!!!!!!!obex/Hellyaw/User/script.lua
+++
@@ -1,4 +0,0 @@
--- only retards change this links
-print('https://discord.gg/b37eKFbkPE <- scriptleaks new server')
--- https://discord.gg/b37eKFbkPE <- scriptleaks new server
-client.exec("go get your new keys and download new loader")
